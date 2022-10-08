f0c3e0d7a09d63bdb22a33ee2d07732111ffc350
Sofi <dornasofia@gmail.com>
2022-10-08 05:54:24 -0300

    primer commit, estructura base, cover, about, studies, experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { CoverComponent } from './components/cover/cover.component';
+import { StudiesComponent } from './components/studies/studies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectComponent,
+    ExperienceComponent,
+    CoverComponent,
+    StudiesComponent
   ],
   imports: [
     BrowserModule
