00043cd1ecbf32764da79da3007bad017c5fb17f
Sofi <dornasofia@gmail.com>
2023-01-08 14:05:16 -0300

    Loading funcionando, falta que no aparezcan los demas componentes

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { finalize, Observable } from 'rxjs';
 import { AutenticacionService } from './autenticacion.service';
 import { GeneralService } from './general.service';
@@ -14,11 +15,15 @@ export class InterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     var currentUser = this.authServ.UsuarioAutenticado;
     this.generalServ.showLoading();
+    // this.ruta.navigate(['/loading']);    
     if (currentUser && currentUser.length > 20){
       req=req.clone({ setHeaders: {Authorization: `Bearer ${currentUser}`}});
     }
     return next.handle(req).pipe(
       finalize(() => this.generalServ.hideLoading())
-    );
+    );  
   }
 }
+
+// this.ruta.navigate(['/home'])
+
